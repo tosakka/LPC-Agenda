e4d06d1d858dd278dc0cce07ab81dd786ce61529
tosakka <plazma54@gmail.com>
2023-03-28 04:39:46 -0400

    todo

---
+++ Practica Agenda/Practica Agenda/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Practica_Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateFeNa = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ckFav = new System.Windows.Forms.CheckBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 116);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 322);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblNombre.Location = new System.Drawing.Point(13, 12);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(16, 30);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(16, 80);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero de Telefono";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(133, 30);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(130, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(130, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fecha Nacimiento";
+            // 
+            // dateFeNa
+            // 
+            this.dateFeNa.Location = new System.Drawing.Point(133, 79);
+            this.dateFeNa.Name = "dateFeNa";
+            this.dateFeNa.Size = new System.Drawing.Size(200, 20);
+            this.dateFeNa.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(242, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Contacto Favorito?";
+            // 
+            // ckFav
+            // 
+            this.ckFav.AutoSize = true;
+            this.ckFav.ForeColor = System.Drawing.SystemColors.Control;
+            this.ckFav.Location = new System.Drawing.Point(245, 32);
+            this.ckFav.Name = "ckFav";
+            this.ckFav.Size = new System.Drawing.Size(35, 17);
+            this.ckFav.TabIndex = 10;
+            this.ckFav.Text = "Si";
+            this.ckFav.UseVisualStyleBackColor = true;
+            this.ckFav.CheckedChanged += new System.EventHandler(this.ckFav_CheckedChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Black;
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAgregar.Location = new System.Drawing.Point(384, 12);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(95, 87);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Black;
+            this.btnEditar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEditar.Location = new System.Drawing.Point(485, 12);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(95, 87);
+            this.btnEditar.TabIndex = 12;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Black;
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEliminar.Location = new System.Drawing.Point(586, 13);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(95, 87);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.Black;
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnConsultar.Location = new System.Drawing.Point(687, 12);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(95, 87);
+            this.btnConsultar.TabIndex = 14;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.ckFav);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateFeNa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateFeNa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox ckFav;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
+
---
+++ Practica Agenda/Practica Agenda/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data.SqlTypes;
+using System.Collections;
+using System.Xml.Linq;
+
+namespace Practica_Agenda
+{
+
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection = new SqlConnection("Data Source=RIN;Initial Catalog=PracticaAgenda;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            consultar();
+            connection.Close();
+        }
+
+        private void consultar()
+        {
+            
+            SqlCommand command = new SqlCommand("SELECT c.id, c.Nombre, c.Apellido, c.FechaNacimiento, t.Numero as 'Numero de Telefono', CASE WHEN f.id IS NULL THEN 'No' ELSE 'Si' END as 'Favorito' FROM Contacto c LEFT JOIN Telefono t ON c.id = t.contacto_id LEFT JOIN Favorito f ON c.id = f.contacto_id", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            Agregar();
+            connection.Close();
+        }
+
+        private void Agregar()
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string fechaNacimiento = dateFeNa.Value.ToString("yyyy-MM-dd");
+            string numeroTelefono = txtNum.Text;
+
+            string consultaInsertarContacto = "INSERT INTO Contacto (nombre, apellido, fechaNacimiento) VALUES (@nombre, @apellido, @fechaNacimiento)";
+            using (SqlCommand cmd = new SqlCommand(consultaInsertarContacto, connection))
+            {
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@apellido", apellido);
+                cmd.Parameters.AddWithValue("@fechaNacimiento", fechaNacimiento);
+                cmd.ExecuteNonQuery();
+            }
+
+            int contactoId = 0;
+            string consultaObtenerId = "SELECT id FROM Contacto WHERE nombre=@nombre AND apellido=@apellido";
+            using (SqlCommand cmd = new SqlCommand(consultaObtenerId, connection))
+            {
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@apellido", apellido);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    contactoId = reader.GetInt32(0);
+                }
+                reader.Close();
+            }
+
+            string consultaInsertarTelefono = "INSERT INTO Telefono (contacto_id, Numero) VALUES (@contacto_id, @Numero)";
+            using (SqlCommand cmd = new SqlCommand(consultaInsertarTelefono, connection))
+            {
+                cmd.Parameters.AddWithValue("@contacto_id", contactoId);
+                cmd.Parameters.AddWithValue("@Numero", numeroTelefono);
+                cmd.ExecuteNonQuery();
+            }
+
+            if (ckFav.Checked)
+            {
+                string consultaInsertarFavorito = "INSERT INTO Favorito (contacto_id) VALUES (@contacto_id)";
+                using (SqlCommand cmd = new SqlCommand(consultaInsertarFavorito, connection))
+                {
+                    cmd.Parameters.AddWithValue("@contacto_id", contactoId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            DateTime fechaNacimiento = dateFeNa.Value;
+            string numeroTelefono = txtNum.Text;
+            bool esFavorito = ckFav.Checked;
+
+            connection.Open();
+            Editar(nombre, apellido, fechaNacimiento, numeroTelefono, esFavorito);
+            connection.Close();
+        }
+
+        private void Editar(string nombre, string apellido, DateTime fechaNacimiento, string numeroTelefono, bool esFavorito)
+        {
+            int id = 0;
+            string consultaObtenerId = "SELECT Contacto.id FROM Contacto INNER JOIN Telefono ON Contacto.id = Telefono.contacto_id WHERE Contacto.nombre = @nombre AND Telefono.Numero = @Numero";
+            using (SqlCommand cmd = new SqlCommand(consultaObtenerId, connection))
+            {
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@Numero", numeroTelefono);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+                reader.Close();
+            }
+
+            if (id > 0)
+            {
+
+                string consultaActualizarContacto = "UPDATE Contacto SET nombre = @nombre, apellido = @apellido, fechaNacimiento = @fechaNacimiento WHERE id = @id";
+                using (SqlCommand cmd = new SqlCommand(consultaActualizarContacto, connection))
+                {
+                    cmd.Parameters.AddWithValue("@nombre", nombre);
+                    cmd.Parameters.AddWithValue("@apellido", apellido);
+                    cmd.Parameters.AddWithValue("@fechaNacimiento", fechaNacimiento.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                string consultaActualizarTelefono = "UPDATE Telefono SET Numero = @Numero WHERE contacto_id = @contacto_id";
+                using (SqlCommand cmd = new SqlCommand(consultaActualizarTelefono, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Numero", numeroTelefono);
+                    cmd.Parameters.AddWithValue("@contacto_id", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                if (esFavorito)
+                {
+
+                    bool yaEsFavorito = false;
+                    string consultaVerificarFavorito = "SELECT COUNT(*) FROM Favorito WHERE contacto_id = @contacto_id";
+                    using (SqlCommand cmd = new SqlCommand(consultaVerificarFavorito, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@contacto_id", id);
+                        yaEsFavorito = ((int)cmd.ExecuteScalar()) > 0;
+                    }
+
+                    if (!yaEsFavorito)
+                    {
+                        string consultaInsertarFavorito = "INSERT INTO Favorito (contacto_id) VALUES (@contacto_id)";
+                        using (SqlCommand cmd = new SqlCommand(consultaInsertarFavorito, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@contacto_id", id);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+                else
+                {
+                    string consultaEliminarFavorito = "DELETE FROM Favorito WHERE contacto_id = @contacto_id";
+                    using (SqlCommand cmd = new SqlCommand(consultaEliminarFavorito, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@contacto_id", id);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+
+            connection.Open();
+            Borrar(nombre, apellido);
+            connection.Close();
+        }
+
+        private void Borrar(string nombre, string apellido)
+        {
+            int id = 0;
+            string consultaObtenerId = "SELECT id FROM Contacto WHERE nombre = @nombre AND apellido = @apellido";
+            using (SqlCommand cmd = new SqlCommand(consultaObtenerId, connection))
+            {
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@apellido", apellido);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+                reader.Close();
+            }
+
+            if (id > 0)
+            {
+                string consultaEliminarFavorito = "DELETE FROM Favorito WHERE contacto_id = @contacto_id";
+                using (SqlCommand cmd = new SqlCommand(consultaEliminarFavorito, connection))
+                {
+                    cmd.Parameters.AddWithValue("@contacto_id", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                string consultaEliminarTelefono = "DELETE FROM Telefono WHERE contacto_id = @contacto_id";
+                using (SqlCommand cmd = new SqlCommand(consultaEliminarTelefono, connection))
+                {
+                    cmd.Parameters.AddWithValue("@contacto_id", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                string consultaEliminarContacto = "DELETE FROM Contacto WHERE id = @id";
+                using (SqlCommand cmd = new SqlCommand(consultaEliminarContacto, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private void ckFav_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
